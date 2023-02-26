e3a3e49c0de4eb9f2d32acee78037c88c69d26bc
David Abenhaim <68952247+davidevenhaim@users.noreply.github.com>
2023-02-26 21:47:38 +0200

    edit post, edit profile, start of socket io implementation

--- dist/src/server.js
+++ dist/src/server.js
@@ -2,39 +2,67 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+// @ Parsing requests and more
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const http_1 = __importDefault(require("http"));
+// @ for .env variables
 const dotenv_1 = __importDefault(require("dotenv"));
+// @ Express
+const express_1 = __importDefault(require("express"));
+// @ DB
+const mongoose_1 = __importDefault(require("mongoose"));
+// @ Swagger
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+// @ Routes
+const auth_route_js_1 = __importDefault(require("./routes/auth_route.js"));
+const post_route_js_1 = __importDefault(require("./routes/post_route.js"));
+const user_route_1 = __importDefault(require("./routes/user_route"));
+const file_route_js_1 = __importDefault(require("./routes/file_route.js"));
+// @ utils
+const utils_1 = require("./socket/utils");
+const message_model_js_1 = __importDefault(require("./models/message_model.js"));
+// @ chat
+const socketio = require('socket.io');
+const io = socketio(http_1.default);
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+app.use(body_parser_1.default.urlencoded({ extended: true, limit: '1mb' }));
+app.use(body_parser_1.default.json());
+app.unsubscribe((0, cookie_parser_1.default)());
 if (process.env.NODE_ENV == 'test') {
     dotenv_1.default.config({ path: './.testenv' });
 }
 else {
     dotenv_1.default.config();
 }
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const http_1 = __importDefault(require("http"));
-const server = http_1.default.createServer(app);
-const body_parser_1 = __importDefault(require("body-parser"));
-app.use(body_parser_1.default.urlencoded({ extended: true, limit: '1mb' }));
-app.use(body_parser_1.default.json());
-const mongoose_1 = __importDefault(require("mongoose"));
 mongoose_1.default.connect(process.env.DATABASE_URL); //,{useNewUrlParser:true})
 const db = mongoose_1.default.connection;
 db.on('error', error => { console.error(error); });
 db.once('open', () => { console.log('connected to mongo DB'); });
 app.use('/public', express_1.default.static('public'));
 app.use('/uploads', express_1.default.static('uploads'));
-const auth_route_js_1 = __importDefault(require("./routes/auth_route.js"));
 app.use('/auth', auth_route_js_1.default);
-const post_route_js_1 = __importDefault(require("./routes/post_route.js"));
 app.use('/post', post_route_js_1.default);
-// import studentRouter from './routes/student_route.js'
-// app.use('/student', studentRouter)
-const user_route_1 = __importDefault(require("./routes/user_route"));
 app.use('/user', user_route_1.default);
-const file_route_js_1 = __importDefault(require("./routes/file_route.js"));
 app.use('/file', file_route_js_1.default);
-const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('sendMessage', (message, senderId, callback) => {
+        const user = (0, utils_1.getUser)(socket.id);
+        const msgData = {
+            userId: user.id,
+            text: message,
+        };
+        console.log("MsgData", msgData);
+        const msg = new message_model_js_1.default({ message, sender: user.id });
+        msg.save().then(res => {
+            io.to().emit('message', res);
+            callback();
+        });
+    });
+});
 if (process.env.NODE_ENV == "development") {
     const options = {
         definition: {
--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,86 @@
+// @ Parsing requests and more
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser';
+import http from 'http';
 
+// @ for .env variables
 import dotenv from 'dotenv'
+
+// @ Express
+import express from 'express'
+
+// @ DB
+import mongoose from "mongoose"
+
+// @ Swagger
+import swaggerUI from "swagger-ui-express"
+import swaggerJsDoc from "swagger-jsdoc"
+
+// @ Routes
+import authRouter from './routes/auth_route.js'
+import postRouter from './routes/post_route.js'
+import userRouter from './routes/user_route';
+import fileRouter from './routes/file_route.js'
+
+// @ utils
+import { getUser } from './socket/utils'
+import Message from './models/message_model.js';
+
+// @ chat stuff
+const socketio = require('socket.io')
+
+const io = socketio(http);
+
+const app = express()
+const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use(bodyParser.json());
+app.unsubscribe(cookieParser());
+
+
 if (process.env.NODE_ENV == 'test') {
     dotenv.config({ path: './.testenv' })
 } else {
     dotenv.config()
 }
 
-import express from 'express'
-const app = express()
-import http from 'http';
-const server = http.createServer(app);
-
-import bodyParser from 'body-parser'
-app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }))
-app.use(bodyParser.json())
-
-import mongoose from "mongoose"
 mongoose.connect(process.env.DATABASE_URL) //,{useNewUrlParser:true})
 const db = mongoose.connection
 db.on('error', error => { console.error(error) })
+
 db.once('open', () => { console.log('connected to mongo DB') })
 
 app.use('/public', express.static('public'))
+
 app.use('/uploads', express.static('uploads'))
 
-import authRouter from './routes/auth_route.js'
 app.use('/auth', authRouter)
 
-import postRouter from './routes/post_route.js'
 app.use('/post', postRouter)
 
-// import studentRouter from './routes/student_route.js'
-// app.use('/student', studentRouter)
-
-import userRouter from './routes/user_route';
 app.use('/user', userRouter);
 
-import fileRouter from './routes/file_route.js'
 app.use('/file', fileRouter)
 
-import swaggerUI from "swagger-ui-express"
-import swaggerJsDoc from "swagger-jsdoc"
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('sendMessage', (message, senderId, callback) => {
+        const user = getUser(socket.id);
+        const msgData = {
+            userId: user.id,
+            text: message,
+        }
+        console.log("MsgData", msgData);
+
+        const msg = new Message({ message, sender: user.id });
+        msg.save().then(res => {
+            io.to().emit('message', res);
+            callback();
+        })
+    })
+})
 
 if (process.env.NODE_ENV == "development") {
     const options = {
